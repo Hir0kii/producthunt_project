bfdeb86c03b255395b782d9d600bded176c4f85f
Hiroki <huchida0622@gmail.com>
2020-05-06 21:51:00 +0900

    finish with the video

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('create/', views.create, name='create'),
+    path('detail/<int:pk>/', views.detail, name='detail'),
+    path('detail/<int:pk>/upvote', views.upvote, name='upvote')
 ]
